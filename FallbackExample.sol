2382efe5a6fc65fa003bcbf45a3ae8610f3b6941
Bami Awotunde <bamideleawotunde@gmail.com>
2022-12-20 03:58:56 +0100

    hardhat deploy, test, config

---
+++ FallbackExample.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+contract FallbackExample {
+    uint256 public result;
+
+    receive() external payable {
+        result = 1;
+    }
+
+    fallback() external payable {
+        result = 2;
+    }
+}
+
+// Explainer from: https://solidity-by-example.org/fallback/
+// Ether is sent to contract
+//      is msg.data empty?
+//          /   \
+//         yes  no
+//         /     \
+//    receive()?  fallback()
+//     /   \
+//   yes   no
+//  /        \
+//receive()  fallback()
